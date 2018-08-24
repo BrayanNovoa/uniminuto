c42f1b82bc15b53361921f3891ab6b82677e55f9
ColProgSoft <bnovoa.linux@gmail.com>
2018-08-24 11:08:20 -0500

    Actualización Semana3

--- Anexos/3_database/3_DML/admin/Inserts/tb_rh.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(1,'O');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(2,'O+');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(3,'A');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(4,'A+');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(5,'B');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(7,'AB');
-INSERT INTO tb_rh (id_rh, grupo_sanguineo) VALUES(6,'B+');
