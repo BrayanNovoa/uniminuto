19f6c3632e4d15c95ca893591f08a7a961a66862
ColProgSoft <bnovoa.linux@gmail.com>
2018-09-16 16:13:13 -0500

    Servlet ready to save any survey

--- Anexos/Etapa3/3_site/SICACEST/src/java/servlet/GuardaRespu.java
+++ Anexos/Etapa3/3_site/SICACEST/src/java/servlet/GuardaRespu.java
@@ -1,24 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Esta aplicación fue desarrollada por Brayan Novoa.
+ * Si desea ponerse en contacto por alguna duda no
+ * dude en escribir al siguiente correo.
+ * bnovoa.linux@gmail.com
  */
 package servlet;
 
-import controller.Consultas;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author b41n
  */
-public class DatosBasicos extends HttpServlet {
+public class GuardaRespu extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,24 +31,11 @@ public class DatosBasicos extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        HttpSession objsesion = request.getSession(false);
-        String usuario= (String)objsesion.getAttribute("SesionUsuario");
-        String names_user = request.getParameter("names_User");
-        String ape1_user = request.getParameter("ape1_User");
-        String ape2_user = request.getParameter("ape2_User");
-        String docu_user = request.getParameter("docu_User");
-        String sex_user = request.getParameter("sex_User");
-        String age_user = request.getParameter("age_User");
-        String depto_user = request.getParameter("depto_User");
-        String ciu_user = request.getParameter("ciu_User");
-        String rh_user = request.getParameter("rh_User");
-        Consultas con = new Consultas();
-        if(con.DatosBasicos(usuario, names_user, ape1_user, ape2_user, docu_user, sex_user, age_user, depto_user, ciu_user, rh_user)){
-            response.sendRedirect("pestanas.jsp");
-        }else{
-            //System.out.println("Error Realizando Encuesta .");
-            response.sendRedirect("errorencuesta.jsp");
+        try (PrintWriter out = response.getWriter()) {
+            String respuestas[] = request.getParameterValues("respuestas");
+            for (int x=0;x<=respuestas.length-1;x++){
+                out.println("<pre>Respuesta: "+respuestas[x]+"</pre>");
+            }
         }
     }
 
